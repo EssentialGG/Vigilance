553f6cb8612101ecddaa1fac59b171c1e81a0612
Sk1er <sk1er@sk1er.club>
2020-01-03 14:00:14 +0200

    Add consumer for state change

--- src/main/kotlin/club/sk1er/vigilance/data/PropertyData.kt
+++ src/main/kotlin/club/sk1er/vigilance/data/PropertyData.kt
@@ -2,11 +2,13 @@ package club.sk1er.vigilance.data
 
 import club.sk1er.vigilance.Vigilant
 import java.lang.reflect.Field
+import java.util.function.Consumer
 
 data class PropertyData(val property: Property, val instance: Vigilant) {
     fun getDataType() = property.type
     var obj: Any? = null
     var field: Field? = null
+    var action: Consumer<Any>? = null
 
     constructor(property: Property, obj: Any, instance: Vigilant) : this(property, instance) {
         this.obj = obj;
@@ -18,13 +20,14 @@ data class PropertyData(val property: Property, val instance: Vigilant) {
 
 
     inline fun <reified T> getValue(): T {
-        if (obj != null) {
-            return obj as T
-        }
+        if (obj != null) return obj as T
+
         return field?.get(instance) as T
     }
 
     fun setValue(value: Any?) {
+        if (value != null) action?.accept(value)
+
         if (obj != null) obj = value
         else field?.set(instance, value)
 
