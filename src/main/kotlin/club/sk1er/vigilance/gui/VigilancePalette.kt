9482963d822c304211c45995858fa1af4d08107a
Matthew Olsson <matthewcolsson@gmail.com>
2020-12-15 15:28:00 -0800

    VigilancePalette: Update some colors

--- src/main/kotlin/club/sk1er/vigilance/gui/VigilancePalette.kt
+++ src/main/kotlin/club/sk1er/vigilance/gui/VigilancePalette.kt
@@ -17,7 +17,7 @@ object VigilancePalette : Vigilant(File("./config/vigilance.toml")) {
     var DARK_DIVIDER = Color(50, 50, 50)
 
     @Property(PropertyType.COLOR, "Scroll Bar", "Color Scheme")
-    var SCROLL_BAR = Color(70, 70, 70)
+    var SCROLL_BAR = Color(45, 45, 45)
 
     @Property(PropertyType.COLOR, "Bright Highlight", "Color Scheme")
     var BRIGHT_HIGHLIGHT = Color(50, 50, 50)
@@ -28,8 +28,11 @@ object VigilancePalette : Vigilant(File("./config/vigilance.toml")) {
     @Property(PropertyType.COLOR, "Dark Highlight", "Color Scheme")
     var DARK_HIGHLIGHT = Color(27, 28, 33)
 
+    @Property(PropertyType.COLOR, "Light Background", "Color Scheme")
+    var LIGHT_BACKGROUND = Color(32, 32, 33)
+
     @Property(PropertyType.COLOR, "Background", "Color Scheme")
-    var BACKGROUND = Color(14, 14, 17)
+    var BACKGROUND = Color(22, 22, 24)
 
     @Property(PropertyType.COLOR, "Dark Background", "Color Scheme")
     var DARK_BACKGROUND = Color(10, 10, 11)
@@ -49,18 +52,17 @@ object VigilancePalette : Vigilant(File("./config/vigilance.toml")) {
     @Property(PropertyType.COLOR, "Warning", "Color Scheme")
     var WARNING = Color(239, 83, 80)
 
-    @Property(PropertyType.COLOR, "Success", "Color Scheme")
-    var SUCCESS = Color(0, 167, 81)
-
     @Property(PropertyType.COLOR, "Accent", "Color Scheme")
     var ACCENT = Color(1, 165, 82)
+
+    @Property(PropertyType.COLOR, "Success", "Color Scheme")
+    var SUCCESS = ACCENT
 
     @Property(PropertyType.COLOR, "Transparent", "Color Scheme")
     var TRANSPARENT = Color(0, 0, 0, 0)
 
     @Property(PropertyType.COLOR, "Disabled", "Color Scheme")
     var DISABLED = Color(80, 80, 80)
-
 
     init {
         initialize()
@@ -69,9 +71,9 @@ object VigilancePalette : Vigilant(File("./config/vigilance.toml")) {
 
 fun Color.withAlpha(alpha: Int) = Color(this.red, this.green, this.blue, alpha)
 fun Color.withAlpha(alpha: Float) = Color(this.red, this.green, this.blue, (alpha * 255).toInt())
+fun Color.invisible() = withAlpha(0)
 operator fun Color.component1() = this.red
 operator fun Color.component2() = this.green
 operator fun Color.component3() = this.blue
 operator fun Color.component4() = this.alpha
 
-fun Color.invisible() = withAlpha(0)
