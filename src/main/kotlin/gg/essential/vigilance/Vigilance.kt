457ff168c45a97a88674824f557a78d4fda10b21
DJtheRedstoner <52044242+DJtheRedstoner@users.noreply.github.com>
2022-08-26 10:58:14 -0400

    remove StencilEffect#enableStencil call from Vigilance#initialize
    This fixes errors in cases where Vigilance#initialize is called from a
    thread that is not the main thread.

--- src/main/kotlin/gg/essential/vigilance/Vigilance.kt
+++ src/main/kotlin/gg/essential/vigilance/Vigilance.kt
@@ -1,16 +1,8 @@
 package gg.essential.vigilance
 
-import gg.essential.elementa.effects.StencilEffect
-
 object Vigilance {
-    private var initialized = false
-
     @JvmStatic
     fun initialize() {
-        if (initialized)
-            return
-
-        initialized = true
-        StencilEffect.enableStencil()
+        // this method no longer serves any purpose but must be kept for api compatibility
     }
 }
