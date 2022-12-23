422716ac5ef8f3a935f466807fc5d19591abc2fe
BlackBeltPanda <dev@pandacraft.org>
2022-12-22 21:18:32 -0500

    Show I/O icons on toggle switches

--- src/main/kotlin/gg/essential/vigilance/gui/settings/SwitchComponent.kt
+++ src/main/kotlin/gg/essential/vigilance/gui/settings/SwitchComponent.kt
@@ -46,7 +46,7 @@ class SwitchComponent(initialState: Boolean) : SettingComponent() {
             x = CenterConstraint()
             y = CenterConstraint()
         }
-    }.bindParent(this, showToggleIndicators and enabled, index = 0)
+    }.bindParent(this, showToggleIndicators and enabled, index = 1)
 
     private val offIndicator by UIContainer().constrain {
         x = 0.pixels(alignOpposite = true)
@@ -57,7 +57,7 @@ class SwitchComponent(initialState: Boolean) : SettingComponent() {
             x = CenterConstraint()
             y = CenterConstraint()
         }
-    }.bindParent(this, showToggleIndicators and !enabled, index = 0)
+    }.bindParent(this, showToggleIndicators and !enabled, index = 1)
 
     init {
         constrain {
